8c34b41b7af48ea3e15efe1f676323a15b50a46d
abr-ya <gamer05@list.ru>
2026-03-13 16:47:24 +0300

    feat: Add auth check to ProductsPage

--- src/pages/products-page.tsx
+++ src/pages/products-page.tsx
@@ -1,20 +1,28 @@
 import { CreateProductForm, ProductsList } from "@/components";
-
+import { useAuth } from "@/providers/auth-provider";
 export const ProductsPage = () => {
+  const { isAuthenticated } = useAuth();
   return (
     <div className="space-y-2">
       <h1 className="text-xl font-bold">Products Page</h1>
-      <div className="flex gap-4">
-        <div className="flex flex-col gap-2 w-1/2">
-          <h2 className="text-lg font-bold">Products list</h2>
-          <ProductsList />
+      { isAuthenticated ? (
+        <div className="flex gap-4">
+          <div className="flex flex-col gap-2 w-1/2">
+            <h2 className="text-lg font-bold">Products list</h2>
+            <ProductsList />
+          </div>
+          <div className="w-1/2">
+            <h2 className="text-lg font-bold">Create product</h2>
+            <p className="text-sm text-muted-foreground">Create a new product to add to the database.</p>
+            <CreateProductForm />
+          </div>
         </div>
-        <div className="w-1/2">
-          <h2 className="text-lg font-bold">Create product</h2>
-          <p className="text-sm text-muted-foreground">Create a new product to add to the database.</p>
-          <CreateProductForm />
+      ) : (
+        <div>
+          <h2 className="text-lg font-bold">You are not authenticated</h2>
+          <p className="text-sm text-muted-foreground">Please login to view this page.</p>
         </div>
-      </div>
+      )}
     </div>
   );
 };
